b6d837033ab18dd1039590dcf5076c47906a655d
aish <aishnikam31@gmail.com>
2024-01-11 01:04:01 +0530

    captcha added successfully

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('cross-fetch');
 const fs = require('fs');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(express.json());
 
-app.post('/send-message', (req, res) => {
-  const { email:senderEmail, message } = req.body;
 
-//   console.log('Received request with data:', req.body); // Log the received data
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://portfolio-aish.web.app'],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-  // Define the message content
-  const content = `Sender's Email: ${senderEmail}\nMessage: ${message}\n\n`;
+app.use(cors(corsOptions));
 
-  // Append the message to a file
-  fs.appendFile('msg.txt', content, (err) => {
-    if (err) {
-      console.error('Error saving message:', err);
-      res.status(500).send(err.toString());
-    } else {
-      console.log('Message saved successfully.');
-      res.status(200).send('Message saved successfully.');
-    }
+
+app.post('/send-message', async (req, res) => {
+  const { email: senderEmail, message, hCaptchaToken } = req.body;
+
+  // Verify hCaptcha token
+  const hCaptchaSecretKey = process.env.HCAPTCHA_SECRET_KEY;
+  const hCaptchaVerifyUrl = 'https://hcaptcha.com/siteverify';
+
+  const verificationResult = await fetch(hCaptchaVerifyUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `secret=${hCaptchaSecretKey}&response=${hCaptchaToken}`,
   });
+
+  const verificationData = await verificationResult.json();
+
+  if (verificationData.success) {
+    // hCaptcha verification successful
+
+    // Define the message content
+    const content = `Sender's Email: ${senderEmail}\nMessage: ${message}\n\n`;
+
+    // Append the message to a file
+    fs.appendFile('msg.txt', content, (err) => {
+      if (err) {
+        console.error('Error saving message:', err);
+        res.status(500).send(err.toString());
+      } else {
+        console.log('Message saved successfully.');
+        res.status(200).json({ message: 'Message saved successfully.' });
+
+      }
+    });
+  } else {
+    // hCaptcha verification failed
+    res.status(403).send('hCaptcha verification failed.');
+  }
 });
 
 app.listen(PORT, () => {
